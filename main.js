ce77f00152fdcd742896af04eb01115c2c32c8e6
Jasper <jasperfong.hm@gmail.com>
2021-10-28 13:15:46 +0800

    base

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 // imports
 const express = require('express');
-const express = require('async');
 const { range } = require('rxjs');
 const { map, filter } = require('rxjs/operators');
 const fs = require('fs-extra');
 const chalk = require('chalk');
 const mongoose = require('mongoose');
-
 var validator = require('validator');
 require('dotenv').config();
-import { format, formatDistance, formatRelative, subDays } from 'date-fns';
-// imports worked correctly
-console.log("Launching..")
+// imports
+const { Client, Intents, Collection} = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+client.commands = new Collection();
+console.log("Launching..");
+// discordjs inititialization
+client.once('ready', () => {
+    console.log(`Bot up at ${Date.getFullYear() + "-" + Date.getMonth() + "-" + Date.getDate() + "\n" + Date.getHours() + ":" + Date.getMinutes()}`)
+})
+client.commands = new Discord.Collection();
+
+const events = fs.readdirSync("./events")
+client.login(process.env.DISCORD_TOKEN);
 
